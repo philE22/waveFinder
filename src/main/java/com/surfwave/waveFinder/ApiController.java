03b7e30b00fd2ca54a77b67b5bce5bbe08116883
philE22 <moonwhlking@gmail.com>
2023-04-07 17:12:37 +0900

    Feat: 파도 예보 그림 페이지 구현

--- src/main/java/com/surfwave/waveFinder/ApiController.java
+++ src/main/java/com/surfwave/waveFinder/ApiController.java
@@ -6,26 +6,57 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/surf")
 @RequiredArgsConstructor
 public class ApiController {
 
     RestTemplate restTemplate = new RestTemplate();
     Gson gson = new Gson();
-    private final String URL = "http://www.khoa.go.kr/api/oceangrid/preOcean/search.do?ServiceKey=OUkrebOp9eqxGzHu5vA3pw==&type=SOKCHO&ResultType=json";
+    private final String URL = "http://www.khoa.go.kr/api/oceangrid/preOcean/search.do?ServiceKey=OUkrebOp9eqxGzHu5vA3pw==&ResultType=json";
+    private final String SOKCHO = "&type=SOKCHO";
+    private final String BUSAN = "&type=BUSAN";
+    private final String TAEAN = "&type=TAEAN";
+    private final String JEJU = "&type=JEJU";
 
 
-    @GetMapping("/surf")
-    public String surf(Model model) {
-        String json = restTemplate.getForObject(URL, String.class);
-        System.out.println(json);
+    @GetMapping("/sokcho")
+    public String sokcho(Model model) {
+        String json = restTemplate.getForObject(URL + SOKCHO, String.class);
         Info info = gson.fromJson(json, Info.class);
         List<SurfInfo> data = info.getResult().getData();
-        System.out.println(data);
+        model.addAttribute("data", data);
+
+        return "/surf";
+    }
+    @GetMapping("/busan")
+    public String busan(Model model) {
+        String json = restTemplate.getForObject(URL + BUSAN, String.class);
+        Info info = gson.fromJson(json, Info.class);
+        List<SurfInfo> data = info.getResult().getData();
+        model.addAttribute("data", data);
+
+        return "/surf";
+    }
+    @GetMapping("/jeju")
+    public String jeju(Model model) {
+        String json = restTemplate.getForObject(URL + JEJU, String.class);
+        Info info = gson.fromJson(json, Info.class);
+        List<SurfInfo> data = info.getResult().getData();
+        model.addAttribute("data", data);
+
+        return "/surf";
+    }
+    @GetMapping("/taean")
+    public String taean(Model model) {
+        String json = restTemplate.getForObject(URL + TAEAN, String.class);
+        Info info = gson.fromJson(json, Info.class);
+        List<SurfInfo> data = info.getResult().getData();
         model.addAttribute("data", data);
 
         return "/surf";
